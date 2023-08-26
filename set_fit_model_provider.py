dde9049ce9212d290dae225e80a8ad822f5bdb88
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2023-08-26 10:11:26 -0700

    refactor

--- set_fit_model_provider.py
+++ set_fit_model_provider.py
@@ -18,40 +18,33 @@ class SetFitModelProvider:
     For the trained (fine-tuned) model, this class also provides serialization/deserialization and inference methods.
     """
 
+    DEFAULT_SHUFFLE_SEED: int = 42
+
     def __init__(
         self,
         model_name: str,
         train_ds: Dataset,
         test_ds: Dataset,
-        selection_range: range | None = None,
         pretrained_model_name: str = "sentence-transformers/paraphrase-mpnet-base-v2",
-        shuffle_seed: int = 42,
+        shuffle_seed: int = DEFAULT_SHUFFLE_SEED,
+        selection_range: range | None = None,
     ) -> None:
         self._model_name: str = model_name
-        self._train_ds: Dataset = train_ds
-        self._test_ds: Dataset = test_ds
-        self._selection_range: range | None = selection_range
         self._pretrained_model_name: str = pretrained_model_name
-        self._shuffle_seed: int = shuffle_seed
 
-        self._model: SetFitModel = SetFitModel.from_pretrained(pretrained_model_name)
-        self._trainer: SetFitTrainer | None = None
+        self._train_ds: Dataset
+        self._test_ds: Dataset
+        self._train_ds, self._test_ds = self._randomize_train_and_test_datasets(
+            train_ds=train_ds,
+            test_ds=test_ds,
+            shuffle_seed=shuffle_seed,
+            selection_range=selection_range,
+        )
 
-    def train(self) -> dict[str, float]:
-        """
-        Trains the initialized SetFitModel using SetFitTrainer on the supplied train and test datasets.
-
-        Returns:
-            Dictionary of metrics values keyed by metric name obtained through running evaluation on the trained model.
-        """
-
-        train_ds: Dataset
-        test_ds: Dataset
-        train_ds, test_ds = self._randomize_train_and_test_datasets()
-
+        model: SetFitModel = SetFitModel.from_pretrained(pretrained_model_name)
         # TODO (8/18/2023): <Alex>The arguments loss_class, batch_size, num_iterations, and num_epochs can be made customizable in the future.</Alex>
-        trainer = SetFitTrainer(
-            model=self._model,
+        self._trainer: SetFitTrainer = SetFitTrainer(
+            model=model,
             train_dataset=train_ds,
             eval_dataset=test_ds,
             loss_class=CosineSimilarityLoss,
@@ -59,16 +52,23 @@ class SetFitModelProvider:
             num_iterations=20,  # Number of text pairs to generate for contrastive learning
             num_epochs=1,  # Number of epochs to use for contrastive learning
         )
-        logger.info(f'Training model "{self._model_name}".')
-        trainer.train()
 
+        self._model_training_complete: bool = False
+        self._model_loaded: bool = False
+
+    def train(self) -> dict[str, float]:
         """
-        Save the "self._trainer" property, because model serialization/deserialization and inference are done through
-        the trainer (not the model) interface.
+        Trains the initialized SetFitModel using the initialized SetFitTrainer on the supplied train and test datasets.
+
+        Returns:
+            Dictionary of metrics values keyed by metric name obtained through running evaluation on the trained model.
         """
-        self._trainer = trainer
+        logger.info(f'Training model "{self._model_name}".')
+        self._trainer.train()
 
         metrics: dict[str, float] = self._trainer.evaluate()
+
+        self._model_training_complete = True
 
         return metrics
 
@@ -77,7 +77,7 @@ class SetFitModelProvider:
         Save (serialize) the trained model to the local filesystem using the model name as the base for the file name.
         """
 
-        if self._trainer is None:
+        if not self._model_training_complete:
             raise ValueError(
                 f'Unable to persist model "{self._model_name}", since it has not yet been trained.'
             )
@@ -93,7 +93,7 @@ class SetFitModelProvider:
         """
 
         do_load: bool
-        if self._trainer is None:
+        if not self._model_training_complete:
             logger.info(
                 f'Loading model "{self._model_name}" from most recently saved version.'
             )
@@ -108,6 +108,7 @@ class SetFitModelProvider:
 
         if do_load:
             self._trainer = joblib.load(f"{self._model_name}.joblib")
+            self._model_loaded = True
 
     def predict(self, text: str) -> int:
         """
@@ -120,19 +121,25 @@ class SetFitModelProvider:
             Binary integer-valued output (0: "negative" or 1: "positive").
         """
 
-        if self._trainer is None:
+        if not (self._model_training_complete or self._model_loaded):
             raise ValueError(
-                f'Unable to use model "{self._model_name}" for inference, since it has not yet been trained.'
+                f'Unable to use model "{self._model_name}" for inference, since it has not yet been trained or loaded.'
             )
 
         return self._trainer.model.predict([text])[0]
 
-    def _randomize_train_and_test_datasets(self) -> tuple[Dataset, Dataset]:
-        train_ds: Dataset = self._train_ds.shuffle(seed=self._shuffle_seed)
-        test_ds: Dataset = self._test_ds.shuffle(seed=self._shuffle_seed)
+    @staticmethod
+    def _randomize_train_and_test_datasets(
+        train_ds: Dataset,
+        test_ds: Dataset,
+        shuffle_seed: int = DEFAULT_SHUFFLE_SEED,
+        selection_range: range | None = None,
+    ) -> tuple[Dataset, Dataset]:
+        train_ds = train_ds.shuffle(seed=shuffle_seed)
+        test_ds = test_ds.shuffle(seed=shuffle_seed)
 
-        if self._selection_range is not None:
-            train_ds = train_ds.select(self._selection_range)
-            test_ds = test_ds.select(self._selection_range)
+        if selection_range is not None:
+            train_ds = train_ds.select(selection_range)
+            test_ds = test_ds.select(selection_range)
 
         return train_ds, test_ds
